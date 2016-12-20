399358dcdc3b595a9d6562f6585b4395b891241c
Loic Talhouarne <talhouar.loic@gmail.com>
2016-12-19 18:23:49 -0600

    * Adding caching.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 const Alexa = require('alexa-sdk');
 const request = require('request');
 
+var cache = { "time" : Date.now(), "value" : "empty"};
+
 const handlers = {
     'LaunchRequest': function () {
         this.emit('GetUpliftingNews');
@@ -10,52 +12,66 @@ const handlers = {
     'IntentRequest': function () {
         this.emit('GetUpliftingNews');
     },
-    'SessionEndedRequest': function () {
-        return;
-    },
+    'SessionEndedRequest': function () {},
     'GetUpliftingNews': function () {
         var reference = this;
 
-        request('https://www.reddit.com/r/upliftingnews/hot.json?sort=hot', function (error, response, body) {
-            console.log("Querying reddit...");
+        var getNonAdminPostEmitted = function(res) {
+            var numberOfPosts = (res.data.children).length;
+            var randomPostIndex = Math.floor(Math.random() * (numberOfPosts));
+            var retryCount = 0;
+            var post = res.data.children[randomPostIndex];
 
-            if (!error && response.statusCode == 200) {
-                var res = JSON.parse(body);
-                var numberOfPosts = (res.data.children).length;
-
-                console.log("Query successful: " + numberOfPosts + " posts returned.");
-
-                var randomPostIndex = Math.floor(Math.random() * (numberOfPosts));
-                var retryCount = 0;
-                var post = res.data.children[randomPostIndex];
-
-                while (retryCount <= 10) {
-                    if (post.data.domain != 'self.UpliftingNews') {
-                        var speechOutput = post.data.title + ". For more information, please visit the Uplifting News subreddit.";
-                        reference.emit(':tell', speechOutput);
-                        break;
-                    } else {
-                        console.warn("Non-admin post not found. Retrying...");
-                        post = res.data.children[Math.floor(Math.random() * (numberOfPosts))];
-                        if (retryCount >= 10) {
-                            reference.emit(':tell', "I could not find uplifting news for you, sorry.");
-                        }
+            while (retryCount <= 10) {
+                if (post.data.domain != 'self.UpliftingNews') {
+                    var speechOutput = post.data.title + ". For more information, please visit the Uplifting News subreddit.";
+                    reference.emit(':tell', speechOutput);
+                    break;
+                } else {
+                    console.warn("Non-admin post not found. Retrying...");
+                    post = res.data.children[Math.floor(Math.random() * (numberOfPosts))];
+                    if (retryCount >= 10) {
+                        reference.emit(':tell', "I could not find uplifting news for you, sorry.");
                     }
                 }
-            } else {
-                console.error("Error reaching service");
-                reference.emit(':tell', "I could not find uplifting news for you, sorry.");
             }
-        });
+        };
+
+        var timeDifference = Date.now() - cache.time;
+
+        if(cache.value == "empty" || timeDifference > 86400000) {
+            request('https://www.reddit.com/r/upliftingnews/hot.json?sort=hot', function (error, response, body) {
+                console.log("Querying reddit...");
+
+                if (!error && response.statusCode == 200) {
+                    var res = JSON.parse(body);
+
+                    //Cache response
+                    cache.value = res;
+                    cache.time = Date.now();
+
+                    getNonAdminPostEmitted(res);
+                } else {
+                    console.error("Error reaching service");
+                    reference.emit(':tell', "I could not find uplifting news for you, sorry.");
+                }
+            });
+        } else {
+            console.log("Value is in the cache. Emitting post.");
+            getNonAdminPostEmitted(cache.value);
+        }
     },
     'Unhandled': function () {
+        this.emit(':tell', "I am sorry, but I am not sure what you asked me.");
+    },
+    'undefined': function () {
         this.emit(':tell', "I am sorry, but I am not sure what you asked me.");
     }
 };
 
 exports.handler = (event, context) => {
     const alexa = Alexa.handler(event, context);
-    alexa.APP_ID = "amzn1.ask.skill.60f22f23-ea8e-4674-b9d1-3f25f5f8346f";
+    alexa.APP_ID = "OMITTED";
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
